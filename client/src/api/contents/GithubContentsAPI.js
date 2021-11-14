423fd393cd9ded4034e5db1ebf4a7e47b858382f
TristinCodingham <tctristinc72@gmail.com>
2021-11-14 13:50:59 +0200

    Update: GithubContentsAPI path

--- client/src/api/contents/GithubContentsAPI.js
+++ client/src/api/contents/GithubContentsAPI.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+import axios from 'axios';
+
+/*
+/repos/:owner/:name/git/
+/repos/:owner/:name/contents/
+/repos/:owner/:name/pulls/
+/repos/:owner/:name/branches/
+/repos/:owner/:name/compare/
+/repos/:owner/:name/commits/
+/repos/:owner/:name/issues/<number>/labels
+*/
+
+axios.get("/.netlify/git/github/contents/")
+    .then(res => console.log(res));
 export default function GithubContentsAPI() {
     return (
         <div>
